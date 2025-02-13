9cb2a27adda5b8fcdb59c58fb48ee60d2f0eae61
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-02-13 22:53:54 +0530

    Constructor

--- src/test1/basic1.java
+++ src/test1/basic1.java
@@ -3,11 +3,16 @@ package test1;
 class Pen{
     String color;
     String type;
+
      public void write(){
          System.out.println("Writing things type-> " + color +" "+ type);
      }
-}
 
+     //argument passing constructor
+     Pen(String color, String type){
+         System.out.println(color+ type);
+     }
+}
 
 class Student{
     String name;
@@ -21,22 +26,21 @@ class Student{
 
 public class basic1 {
     public static void main(String[] args) {
-        Pen p1 = new Pen();
-        p1.color="blue";
-        p1.type= "ball pen";
-        p1.write();
 
-        Pen p2 = new Pen();
-        p2.color="black";
-        p2.type="Gel pen";
+        //Constructor Call" through an instance
+//        Pen p1 = new Pen();
+//        p1.color="blue";
+//        p1.type= "ball pen";
+//        p1.write();
+
+        Pen p2 = new Pen("black","Gel Pen");
         p2.write();
-
-
         System.out.println("----------------------------------");
 
         Student s1 = new Student();
         s1.name = "Satyajit";
         s1.age= 24;
         s1.nameinfo();
+
     }
 }
